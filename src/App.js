58f70fda83657ed32a88ba0cbc53b065a1b523b0
Nishanthreddy <99809974+heisenlord@users.noreply.github.com>
2025-01-04 22:20:39 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Footer from "./components/Footer/footer";
 import Sidecar from "./components/Sidecar/sidecar";
 import Subwork from "./components/Works/Subworks/subwork";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { Analytics } from "@vercel/analytics/react"
+
 import './App.css';
 
 
